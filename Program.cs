b787416b6d65ab898bb6f75b0bac1b46d49f03af
Byron McDaniel <byronkmcdaniel@gmail.com>
2020-09-01 15:42:09 -0400

    ApiClient

--- Program.cs
+++ Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
 
 namespace ApiClient
 {
+
+  class joke
+  {
+   public int id { get; set; } 
+   public string type { get; set; }
+   public string setup { get; set; }
+   public string punchline { get; set; }
+
+  }
   class Program
   {
-    static void Main(string[] args)
+
+  
+
+    static async System.Threading.Tasks.Task Main(string[] args)
     {
-      Console.WriteLine("Welcome to C#");
+      var client = new HttpClient();
+
+      var responseAsStream = await client.GetStreamAsync("https://official-joke-api.appspot.com/random_ten");
+
+       var jokes = await JsonSerializer.DeserializeAsync<List<joke>>(responseAsStream);
+
+       //foreach (var joke in jokes)
+                  
+                   
+        //Console.WriteLine($"The joke number is {joke.id} the joke type is {joke.type} the set up is {joke.setup} and the punch line goes {joke.punchline}.  ");
+
+        
+        
+        Console.WriteLine($"there ar {jokes.Count()} total jokes");
+
+                          
+
+
+
+      
+
+
+
     }
   }
 }
